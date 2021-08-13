610cab3e4c55d950aec0c7f990ae60f8196fe336
Fernando Santana <fernandodasilvasantana@hotmail.com>
2021-08-13 00:01:05 -0300

    Criação da opção Delete do vendedor

--- WebMvcNetCore/Controllers/VendedoresController.cs
+++ WebMvcNetCore/Controllers/VendedoresController.cs
@@ -41,5 +41,30 @@ namespace WebMvcNetCore.Controllers
             _servicoVendedor.Insert(vendedor);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _servicoVendedor.FindById(id.Value);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _servicoVendedor.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
